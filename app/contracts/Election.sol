4f3e3ab05229738f3b11f4c799d6a69237161d94
kyle <kamon021@student.wethinkcode.co.za>
2022-12-01 12:34:19 +0200

    first commit

---
+++ app/contracts/Election.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.2;
+
+contract Election {
+    //constructor
+    //candidate
+    //vote count
+    //read candidate
+
+    string public candidate; 
+
+    //constructor
+    constructor() public  {
+        candidate = "Candidate 1";
+    }
+
+
+}
